69fceaf5a041395eb3df174dab888ae87031a15c
Serafim Junior <serafimmotajr@gmail.com>
2020-10-24 13:23:30 -0300

    serviço help desk

--- helpdeskservice/src/main/java/com/streaming/helpdesk/repository/HelpDeskRepository.java
+++ helpdeskservice/src/main/java/com/streaming/helpdesk/repository/HelpDeskRepository.java
@@ -1,5 +1,7 @@
 package com.streaming.helpdesk.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.streaming.helpdesk.model.*;
@@ -7,7 +9,7 @@ import com.streaming.helpdesk.model.*;
 import antlr.collections.List;
 
 @Repository
-public interface HelpDeskRepository extends JpaRepository<HelpDesk,String> {
+public interface HelpDeskRepository extends JpaRepository<HelpDesk,Integer> {
 
-	
+
 }
