d5eee1b1f881fb7bc54ebaf6eb98627d6f9013bc
Claudio Araujo Junior <31231752+juniorescaj@users.noreply.github.com>
2020-11-01 22:41:18 -0300

    change service discovery package name

--- servicediscovery/src/main/java/com/streaming/servicediscovery/ServiceDiscoveryApplication.java
+++ servicediscovery/src/main/java/com/streaming/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.netflix.servicediscovery;
+package com.streaming.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
